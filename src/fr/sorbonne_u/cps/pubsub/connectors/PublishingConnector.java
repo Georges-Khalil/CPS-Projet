76e0e9f431e841b243db684c63a453e68315d6c9
28726514 <come.lance-perlick@etu.sorbonne-universite.fr>
2026-02-13 16:52:04 +0100

    Implémentation des connecteurs simples

--- src/fr/sorbonne_u/cps/pubsub/connectors/PublishingConnector.java
+++ src/fr/sorbonne_u/cps/pubsub/connectors/PublishingConnector.java
@@ -1,11 +1,24 @@
 package fr.sorbonne_u.cps.pubsub.connectors;
 
 import fr.sorbonne_u.components.connectors.AbstractConnector;
+import fr.sorbonne_u.cps.pubsub.interfaces.MessageI;
 import fr.sorbonne_u.cps.pubsub.interfaces.PublishingCI;
+
+import java.util.ArrayList;
 
 public class PublishingConnector
         extends AbstractConnector
         implements PublishingCI
 {
 
+    @Override
+    public void publish(String receptionPortURI, String channel, MessageI message) throws Exception {
+        ((PublishingCI) this.offering).publish(receptionPortURI, channel, message);
+
+    }
+
+    @Override
+    public void publish(String receptionPortURI, String channel, ArrayList<MessageI> messages) throws Exception {
+        ((PublishingCI) this.offering).publish(receptionPortURI, channel, messages);
+    }
 }
