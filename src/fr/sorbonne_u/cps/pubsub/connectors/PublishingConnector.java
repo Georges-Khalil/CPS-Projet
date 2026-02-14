23a4447a3b1bff7a9428dcbe42a1b6925d61ae7b
Georges-Khalil <georgesghassankhalil@gmail.com>
2026-02-14 04:45:53 +0100

    test

--- src/fr/sorbonne_u/cps/pubsub/connectors/PublishingConnector.java
+++ src/fr/sorbonne_u/cps/pubsub/connectors/PublishingConnector.java
@@ -1,11 +1,26 @@
 package fr.sorbonne_u.cps.pubsub.connectors;
 
+import java.util.ArrayList;
+
 import fr.sorbonne_u.components.connectors.AbstractConnector;
+import fr.sorbonne_u.cps.pubsub.interfaces.MessageI;
 import fr.sorbonne_u.cps.pubsub.interfaces.PublishingCI;
 
 public class PublishingConnector
         extends AbstractConnector
         implements PublishingCI
 {
+
+	@Override
+	public void publish(String receptionPortURI, String channel, MessageI message) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void publish(String receptionPortURI, String channel, ArrayList<MessageI> messages) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
